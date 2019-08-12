8c11744753825720ef11657a138af640a0305b93
lcedenog <lcedenog@est.ups.edu.ec>
2019-08-12 02:22:40 -0500

    Panel Opener
    Panel Opener

---
+++ MY_CATHEDRAL/Gotourl/gotourl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gotourl : MonoBehaviour
+{
+    public string miweb;
+    void Start()
+    {
+
+    }
+
+    public void realprogramers(){
+        Application.OpenURL(miweb);
+ 
+}
+
+  
+}
