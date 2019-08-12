a917dfb23ae19951078cd7c60f8980379ac67cc4
lcedenog <lcedenog@est.ups.edu.ec>
2019-08-12 02:08:15 -0500

    Create splash.cs
    SplashScreen

---
+++ MY_CATHEDRAL/SPLASHSCREEN/splash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splash : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("loading");
+        
+    }
+
+}
