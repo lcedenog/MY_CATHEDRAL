327295327fd3149e8088703e4ff1686a2bcdd63f
lcedenog <lcedenog@est.ups.edu.ec>
2019-08-12 02:04:21 -0500

    Create Menu
    Create Menu "My Cathedral" APP

---
+++ MY_CATHEDRAL/MENU/menu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("main_menu");
+    }
+
+    public void GoToBienvenidos()
+    {
+        SceneManager.LoadScene("Bienvenidos");
+    }
+
+   
+
+    public void GoToARCamera()
+    {
+        SceneManager.LoadScene("altares");
+    }
+
+    public void GoToMaria()
+    {
+        SceneManager.LoadScene("maria");
+    }
+
+    public void GoToJesus()
+    {
+        SceneManager.LoadScene("jesus");
+    }
+
+    public void GoToNarcisa()
+    {
+        SceneManager.LoadScene("narcisa");
+    }
+
+    public void GoToDonBosco()
+    {
+       SceneManager.LoadScene("donbosco");
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+}
